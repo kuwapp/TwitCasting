77061b678e17c47688fac303446e7eda52f66548
hiraok <khirao.dev.work@gmail.com>
2018-09-12 01:17:55 +0900

    modify

--- app/src/main/java/hiraok/dev/jp/twitcasting/api/AuthHeader.kt
+++ app/src/main/java/hiraok/dev/jp/twitcasting/api/AuthHeader.kt
@@ -1,5 +1,32 @@
 package hiraok.dev.jp.twitcasting.api
 
+import android.util.Base64
+import hiraok.dev.jp.twitcasting.BuildConfig
+import okhttp3.Headers
+
 object AuthHeader {
+
+    fun createHeader(accessToken: String? = null): Headers {
+        val map = mapOf(Pair("X-Api-Version", "2.0"),
+                Pair("Authorization", authorizationValue(accessToken)))
+        return Headers.of(map)
+    }
+
+    private fun authorizationValue(accessToken: String?): String {
+        return accessToken?.let { accessTokenValue(it) } ?: clientIdSecretValue()
+    }
+
+    private fun accessTokenValue(accessToken: String): String {
+        return " Bearer " + accessToken;
+    }
+
+    private fun clientIdSecretValue(): String {
+        return " Basic " + base64ClientIdSecret()
+    }
+
+    private fun base64ClientIdSecret(): String {
+        val clientIdAndSecret = "chiraok.2d438d2efb56736aea7c282f6e1904b8244d707dff660a03c61c308aac814e82" + ":" + "f4075f9ef7e054ee7fa70817fa70af775924262202aa9e1294be33b992c17a49"
+        return Base64.encodeToString(clientIdAndSecret.toByteArray(), Base64.NO_WRAP)
+    }
 
 }
--- app/src/main/java/hiraok/dev/jp/twitcasting/api/repository/TwitCastingRepository.kt
+++ app/src/main/java/hiraok/dev/jp/twitcasting/api/repository/TwitCastingRepository.kt
@@ -2,36 +2,26 @@ package hiraok.dev.jp.twitcasting.api.repository
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
+import hiraok.dev.jp.twitcasting.api.AuthHeader
 import hiraok.dev.jp.twitcasting.api.TwitCastingService
 import hiraok.dev.jp.twitcasting.api.model.NewLive
+import hiraok.dev.jp.twitcasting.di.NetworkModule
 import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 
 object TwitCastingRepository {
 
-    private var service: TwitCastingService
-
-    init {
-
-        val client = OkHttpClient.Builder().addInterceptor {
-            val header = AuthHeader.makeHeader(it) ?: AuthHeader.makeHeader()
-            val request = it.request().newBuilder().headers(header).build()
-            it.proceed(request)
-        }.build()
-
-        val retrofit = Retrofit.Builder().baseUrl("https://apiv2.twitcasting.tv").addConverterFactory(GsonConverterFactory.create()).build()
-        service = retrofit.create(TwitCastingService::class.java)
-    }
-
+    @Inject
+    lateinit var networkModule: NetworkModule
 
     fun getNewLiveList(): LiveData<List<NewLive>> {
-        var data: MutableLiveData<List<NewLive>> = MutableLiveData()
-
-        service.getNewLiveList("ja").enqueue(object : Callback<List<NewLive>> {
+        val data: MutableLiveData<List<NewLive>> = MutableLiveData()
+        networkModule.provideTwitCastingService(networkModule.provideRetrofit(networkModule.provideOkHttpClient())).getNewLiveList("ja").enqueue(object : Callback<List<NewLive>> {
             override fun onResponse(call: Call<List<NewLive>>, response: Response<List<NewLive>>) {
                 //To change body of created functions use File | Settings | File
                 // Templates.
